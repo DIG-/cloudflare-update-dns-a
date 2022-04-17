6b70ead7bde3750762487d14bf5f4f792743d8e3
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-04-17 17:20:20 -0300

    Implemeneted get zone dns record to compare ip

--- src/cloudflare-update-dns/__init__.py
+++ src/cloudflare-update-dns/__init__.py
@@ -1,6 +1,9 @@
 from getopt import gnu_getopt
+from pprint import pprint
 from sys import argv, exit
 from typing import Optional
+
+from CloudFlare import CloudFlare
 
 from .ip import get_ip as get_real_ip
 
@@ -8,9 +11,10 @@ from .ip import get_ip as get_real_ip
 def main():
     email: Optional[str] = None
     token: Optional[str] = None
+    zone: Optional[str] = None
     dns_a: Optional[str] = None
     try:
-        options, extras = gnu_getopt(argv[1:], "e:t:", ["email=", "token=", "dns-a="])
+        options, extras = gnu_getopt(argv[1:], "e:t:z:", ["email=", "token=", "zone=", "dns-a="])
     except BaseException as e:
         print("Failed to parse options")
         print(e)
@@ -21,6 +25,8 @@ def main():
             email = value
         elif option in ("-t", "--token"):
             token = value
+        elif option in ("-z", "--zone"):
+            zone = value
         elif option == "--dns-a":
             dns_a = value
 
@@ -28,10 +34,45 @@ def main():
         print("Require API Token or API Key")
         exit(2)
 
+    if zone is None:
+        print("Require zone")
+        exit(2)
+
+    if dns_a is None:
+        print("Require dns-a value to update")
+        exit(2)
+
+    cf = CloudFlare(email=email, token=token)
+    try:
+        zones = cf.zones.get(params={"name": zone, "per_page": 1})
+    except BaseException as e:
+        print("Failed to get zones from CloudFlare")
+        print(e)
+        exit(3)
+    if len(zones) <= 0:
+        print("No zone found")
+        exit(3)
+    zone = zones[0]
+
+    try:
+        records = cf.zones.dns_records.get(zone["id"], params={"type": "A", "name": dns_a})
+    except BaseException as e:
+        print("Failed to get dns records from CloudFlare")
+        print(e)
+        exit(3)
+    
+    if len(records) <=0:
+        print("Records not found")
+        exit(3)
+    
+    record = records[0]
+    record_id = record["content"]
+
     try:
         ip_real = get_real_ip()
     except BaseException as e:
         print("Failed to get current ip")
         print(e)
         exit(3)
+    print(record_id)
     print(ip_real)
